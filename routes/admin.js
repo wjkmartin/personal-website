a7f9c7006da4a33f02cf6f064fef5449d3963be0
William Martin <wjkmartin@gmail.com>
2019-04-04 06:54:08 -0700

    added dynamic page title via EJS

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ var express = require('express')
 var router = express.Router()
 
  router.get('/', function(req, res) {
-     res.render('admin');
+     res.render('admin', {docTitle: 'Manage site'});
  });
 
 router.get('/create-post', function(req, res) {
-    res.render('create-post');
+    res.render('create-post', {docTitle: 'Create a new post'});
 });
 
 module.exports = router;
--- routes/public.js
+++ routes/public.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', {docTitle: 'Will Martin\'s Portfolio and Blog'});
 });
 
 router.get('/index', function(req, res) {
@@ -10,15 +10,15 @@ router.get('/index', function(req, res) {
 })
 
 router.get('/blog', function(req, res) {
-    res.render('blog');
+    res.render('blog', {docTitle: 'Will Martin\'s Blog'});
 });
 
 router.get('/portfolio', function(req, res) {
-    res.render('portfolio');
+    res.render('portfolio', {docTitle: 'Will Martin\'s Portfolio'});
 });
 
 router.get('/login', function(req, res) {
-    res.render('login');
+    res.render('login', {docTitle: 'Admin Login'});
 });
 
 module.exports = router;
