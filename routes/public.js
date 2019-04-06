a1c59c486a20a506a5820b171eac8b006b4c4023
William Martin <wjkmartin@gmail.com>
2019-04-06 12:30:07 -0700

    completely remade header

--- routes/public.js
+++ routes/public.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 
 router.get('/', function(req, res) {
-    res.render('index', {docTitle: 'Will Martin\'s Portfolio and Blog'});
+    res.render('index', {docTitle: 'Will Martin\'s Portfolio and Blog', path: 'index'});
 });
 
 router.get('/index', function(req, res) {
@@ -10,15 +10,23 @@ router.get('/index', function(req, res) {
 })
 
 router.get('/blog', function(req, res) {
-    res.render('blog', {docTitle: 'Will Martin\'s Blog'});
+    res.render('blog', {docTitle: 'Will Martin | Blog', path: 'blog'});
 });
 
 router.get('/portfolio', function(req, res) {
-    res.render('portfolio', {docTitle: 'Will Martin\'s Portfolio'});
+    res.render('portfolio', {docTitle: 'Will Martin | Portfolio', path: 'portfolio'});
+});
+
+router.get('/about', function(req, res) {
+    res.render('about', {docTitle: 'Will Martin | About', path: 'about'});
+});
+
+router.get('/contact', function(req, res) {
+    res.render('contact', {docTitle: 'Will Martin | Contact', path: 'contact'});
 });
 
 router.get('/login', function(req, res) {
-    res.render('login', {docTitle: 'Admin Login'});
+    res.render('login', {docTitle: 'Will Martin | Login'});
 });
 
 module.exports = router;
